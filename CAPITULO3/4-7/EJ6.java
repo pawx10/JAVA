1173d34db9cd4c5e0ef3dda3861b89250d6b01f5
pawx10 <arroyopablo2004@gmail.com>
2022-11-15 19:30:59 +0100

    nuevo ej

--- CAPITULO3/4-7/EJ6.java
+++ CAPITULO3/4-7/EJ6.java
@@ -1,9 +1,22 @@
 /**
-* area rectangulo
+* area triangulo
 *
 * @author Pablo Arroyo
 */
 
 public class EJ6 {
-    
+    public static void main(String[]args){
+        String area;
+        System.out.print("Escribe la base ");
+       area=System.console().readLine();
+        int base;
+        base= Integer.parseInt(area);
+        System.out.print("Escribe la altura ");
+        area=System.console().readLine();
+        int altura;
+        altura= Integer.parseInt(area);
+        System.out.print("El area es: "+((base*altura)*2));
+        
+
+}
 }
