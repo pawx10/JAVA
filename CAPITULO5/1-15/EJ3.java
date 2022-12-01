7af4b28aa301c1d8b0d42428fb083ef3bc2003b8
pawx10 <arroyopablo2004@gmail.com>
2022-12-01 19:16:10 +0100

    a

---
+++ CAPITULO5/1-15/EJ3.java
@@ -0,0 +1,11 @@
+/**
+ * @author Pablo Arroyo
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while.
+ */
+public class EJ3 {
+    public static void main(String[]args){
+        
+
+    }
+    
+}
