b8aa8e417567fea8b6f45f7d68d7f0daa8f78fe6
pawx10 <arroyopablo2004@gmail.com>
2022-12-16 16:55:52 +0100

    A

---
+++ CAPITULO5/MUCHOSEJ/EJ37.java
@@ -0,0 +1,31 @@
+public class EJ37 {
+    public static void main (String[] args) {
+    long num;
+    do {
+    System.out.print("Introduce un número entero positivo: ");
+    num = Integer.parseInt(System.console().readLine());
+    } while (num < 1);
+    System.out.print(num + " = ");
+    // Cuenta los números y calcula el reves
+    long aux = num;
+    long numDig = 0;
+    long numReves = 0;
+    while (aux > 0){
+    numReves = ((numReves*10) + (aux % 10));
+    aux /=10;
+    numDig++;
+    }
+    long cifra = 0;
+    // Separa las cifras y escribe los palitos
+    for (long i = 0; i < numDig; i++){
+    cifra = numReves%10;
+    numReves /= 10;
+    for (int j = 0; j < cifra; j++){
+    System.out.print("|");
+    }
+    if (i < numDig-1){
+    System.out.print("-");
+    }
+    }
+    }
+    }
